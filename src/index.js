7b23da59774e1205dfa282a0b8d1bbb56190696e
Eray Can Topcu <theressam52@gmail.com>
2024-06-01 17:08:04 +0300

    badge

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import "./style/_global.css";
 import drawer from "./components/drawer";
 import modal from "./components/modal";
 import accordion from "./components/accordion";
-import badge from "./components/breadcrumb";
 import breadcrumb from "./components/breadcrumb";
 
-export { drawer, modal, accordion, badge, breadcrumb };
+export { drawer, modal, accordion, breadcrumb };
